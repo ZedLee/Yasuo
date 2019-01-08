007b8079f92903e4b166be776ad859085f1da6c8
Zed Li <zed_li@intsig.net>
2019-01-08 18:12:40 +0800

    epoll added

--- src/Epoll.cpp
+++ src/Epoll.cpp
@@ -9,7 +9,107 @@
  *
  */
 
-namespace dWS
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#include "Epoll.h"
+
+namespace yasuo
 {
-    
+    Epoll::Epoll()
+    {
+		sockaddr_in sin = { 0 };
+
+		sin.sin_addr.s_addr = inet_addr(this->m_strIP.c_str());
+		sin.sin_family = AF_INET;
+		sin.sin_port = htons(this->m_iPort);
+
+		m_iListenFd = socket(AF_INET, SOCK_STREAM, 0);
+		if (0 >= m_iListenFd)
+        {
+			throw std::runtime_error("socket() failed, error code: " + std::to_string(errno));
+        }
+
+		if (bind(m_iListenFd, reinterpret_cast<sockaddr *>(&sin), sizeof(sin)))
+        {
+			throw std::runtime_error("bind() failed, error code: " + std::to_string(errno));
+        }
+
+		if (false == SetNonblocking(m_iListenFd))
+        {
+			throw std::runtime_error("SetNonBlocking() failed, error code: " + std::to_string(errno));
+        }
+
+		if (-1 == listen(m_iListenFd, SOMAXCONN))
+        {
+			throw std::runtime_error("listen() failed, error code: " + std::to_string(errno));
+        }
+
+		m_iEpfd = epoll_create1(0);
+		if (-1 == m_iEpfd)
+        {
+			throw std::runtime_error("epoll_create1() failed, error code: " + std::to_string(errno));
+        }
+
+		epoll_event e_event;
+		e_event.events = EPOLLIN;
+		e_event.data.fd = m_iListenFd;
+
+		if (-1 == epoll_ctl(m_iEpfd, EPOLL_CTL_ADD, m_iListenFd, &e_event))
+        {
+			throw std::runtime_error("epoll_ctl() failed, error code: " + std::to_string(errno));
+        }
+
+    };
+
+    Epoll::~Epoll()
+    {
+        if (-1 != m_iEpfd) 
+        {
+            close(m_iEpfd);
+        }
+        if (-1 != m_iListenFd) 
+        {
+            close(m_iListenFd);
+        }
+    }
+
+    Epoll::start()
+    {
+        if (EPOLL_STATUS::STOP == m_iEpollStatus)
+        {
+            m_iEpollStatus = EPOLL_STATUS::START
+            doEpoll();
+        }
+    }
+
+    Epoll::doEpoll()
+    {
+        while (EPOLL_STATUS::START == m_iEpollStatus)
+        {
+            int numFdReady = epoll_wait(m_iEpfd, m_pEvents, m_iNumEvents, 0);
+            for (int i = 0; i < numFdReady; i++) 
+            {
+                Poll *poll = (Poll *) readyEvents[i].data.ptr;
+                int status = -bool(readyEvents[i].events & EPOLLERR);
+                callbacks[poll->state.cbIndex](poll, status, readyEvents[i].events);
+            }
+        }
+    }
+
+    bool Epoll::SetNonblocking(int fd)
+	{
+		int flags = fcntl(fd, F_GETFL, 0);
+		if (-1 == flags)
+			return false;
+
+		flags |= O_NONBLOCK;
+
+		if (-1 == fcntl(fd, F_SETFL, flags))
+			return false;
+
+		return true;
+	}
 }
--- src/Epoll.h
+++ src/Epoll.h
@@ -11,28 +11,57 @@
 #ifndef EPOLL_H
 #define EPOLL_H
 
-namespace dWS
+#include <sys/epoll.h>
+#include <string>
+
+namespace yasuo
 {
 
     class Epoll 
     {
     public:
+        Epoll(std::string ip, int port) : m_strIP(ip), m_iPort(port) { Epoll(); };
+
         Epoll();
        
         ~Epoll();
 
-        int startEpoll();
+        int start();
+
+        int stop();
+
+    protected:
+        // Set a file descriptor as non blocking mode
+    	bool SetNonblocking(int fd);
+
+        // Do epoll
+        void doEpoll();
 
     private:
 
         // epoll file description
         int m_iEpfd;
 
-        // max connections
-        int m_iMaxConn = 1024000;
-
         // amount of events
-        int m_iNumEvents = 1024;
+        const int m_iNumEvents = 1024;
+
+        // Events that needed to process
+        epoll_event m_pEvents[m_iNumEvents];
+
+        // Port that needed to listen
+        int m_iPort = 80;
+
+        // IP address needed to bind
+        std::string m_strIP = "0.0.0.0"
+
+        int m_iListenFd;
+
+        enum EPOLL_STATUS {
+            STOP = 0,
+            START
+        }
+
+        int m_iEpollStatus = EPOLL_STATUS::STOP;
 
     };
 
